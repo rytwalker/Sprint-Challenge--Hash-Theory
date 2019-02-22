7bcc2546fdcde8d1244e9f5ed2a519a3f201de15
Ryan Walker <rytwalker@gmail.com>
2019-02-22 12:36:58 -0500

    ex2 - tests pass

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -9,19 +9,34 @@ char **reconstruct_trip(Ticket **tickets, int length)
   HashTable *ht = create_hash_table(16);
   char **route = malloc(length * sizeof(char *));
 
-  // YOUR CODE HERE
+  // ADD to ht key: source, value: destination
+  for (int i = 0; i < length; i++)
+  {
+    hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
+  }
+
+  // Init route start by getting first location from "NONE" key;
+  char *location = strdup(hash_table_retrieve(ht, "NONE"));
+  // ADD TO ROUTE
+  for (int i = 0; i < length; i++)
+  {
+    route[i] = strdup(location);
+    location = hash_table_retrieve(ht, location);
+  }
+
+  // Free up hash table memory
+  destroy_hash_table(ht);
 
   return route;
 }
 
 void print_route(char **route, int length)
 {
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     printf("%s\n", route[i]);
   }
 }
-
-
 
 #ifndef TESTING
 int main(void)
